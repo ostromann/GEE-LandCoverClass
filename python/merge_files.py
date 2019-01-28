6c9e8b79fb3a71db135df668996b24a87d3dd0e7
ostromann <40261070+ostromann@users.noreply.github.com>
2019-01-28 13:31:12 +0100

    Add files via upload

--- python/merge_files.py
+++ python/merge_files.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Merge multiple CSV files that have the same prefix separated by '-'.
 
 This script merges all CSV files in PATH that share the same prefix,
@@ -14,16 +13,17 @@ Example:
 Attributes:
     None.
 Todo:
-    * Pass PATH as argument (is it more convenient?)
+    * Pass path as attribute (convenient?)
 """
 
 import glob
 import random
 import os
+from os import mkdir
 import pandas as pd
 
 #GLOBALS
-PATH="../exports"
+PATH="demo/ee_exports"
    
 def find_filesets(path="."):
     csv_files = {}
@@ -32,22 +32,25 @@ def find_filesets(path="."):
         csv_files.setdefault(key, []).append(name)
 
     for key,filelist in csv_files.items(): 
-        print(key, filelist)
+        print(key)
+        for file in filelist:
+            print(file)
         create_merged_csv(key, filelist)
 
 def create_merged_csv(key, filelist):
     with open('{}.csv'.format(key), 'w+b') as outfile:
         df1 = pd.DataFrame()
         for filename in filelist:
-            df2 = pd.read_csv(filename).drop(['system:index', 'ID', '.geo', 'class'], axis=1, errors='ignore')
+            df2 = pd.read_csv(filename).drop(['FID', 'ID', '.geo', 'class'], axis=1, errors='ignore')
             if df1.empty:
-                df1 = df2
-                
+                df1 = df2               
             else:
-                #df1 = pd.merge(df1,df2, on='system:index', how='left')
                 df1 = pd.concat([df1,df2])
-                df1 = df1.drop_duplicates(subset='FID', keep='last')
-        df1.to_csv('{}.csv'.format(key), index=False)
+                df1 = df1.drop_duplicates(subset='system:index', keep='last')
+        new_path = PATH + '/merged'
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        df1.to_csv(new_path + '/' + '{}.csv'.format(key), index=False)
 
 if __name__ == "__main__":
     find_filesets(PATH)
